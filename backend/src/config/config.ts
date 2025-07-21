e0eaa44be6923539926578c5b0757a85c274bef5
pandajakob <jakobm.dev@gmail.com>
2025-07-21 18:00:01 +0200

    check if works

--- backend/src/config/config.ts
+++ backend/src/config/config.ts
@@ -8,7 +8,7 @@ interface Config {
 }
 
 const config: Config = {
-  port: Number(process.env.PORT) || 3000,
+  port: Number(process.env.PORT),
   nodeEnv: process.env.NODE_ENV || 'development',
 };
 
